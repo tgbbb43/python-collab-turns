e8840ca5e2b5e95a4ded36a31810b721d2f0efad
Yotam Betesh <yotambt1@gmail.com>
2026-03-26 19:40:23 +0200

    YOSI

--- utils.py
+++ utils.py
@@ -7,8 +7,4 @@ def read_entries():
     with open("data.txt", "r") as f:
         return f.readlines()
     
-    def delete_last_entry():
-    with open("data.txt", "r") as f:
-        lines = f.readlines()
-    with open("data.txt", "w") as f:
-        f.writelines(lines[:-1])
+    print ("BIBI")
